4cbe97b4f369f03329dfcdd8cdec96d90218b6e7
fillerup <mats.fillerup@gmail.com>
2014-10-30 22:14:08 +0100

    Added the hello world.

---
+++ src/main/java/nl/velveto/HelloVelveto.java
@@ -0,0 +1,12 @@
+package nl.velveto;
+
+import java.lang.System;
+
+public class HelloVelveto {
+
+    public static void main(String[] argd) {
+        System.out.println("Hello Velveto");
+    }
+
+}
+
